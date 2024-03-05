ca5074499a3475939c3e55c7c4f395d00ca39766
Machi-san <maxibengochea67@gmail.com>
2024-03-05 18:42:25 -0500

    fix process corpus and query

--- src/code/process_corpus.py
+++ src/code/process_corpus.py
@@ -2,12 +2,13 @@
 import ir_datasets
 import spacy
 from lex_process import *
+from utils import list_to_set
 
 #cargar el corpus
 def load() -> list:
   datasets = ir_datasets.load("cranfield")
   docs = [doc.text for doc in datasets.docs_iter()]
-  return docs
+  return docs[:3]
 
 #devolver el corpus parseado
 def parse_corpus() -> list:
@@ -20,40 +21,6 @@ def parse_corpus() -> list:
   rem_noise = remove_noise_spacy(tok)
   rem_sw = remove_stopwords_spacy(rem_noise)
   morph_red = morphological_reduction_spacy(rem_sw)
-  return morph_red
+  return list(map(list_to_set, morph_red))
 
-#devolver los documentos con sus respectivos valores en bits
-def docs_values() -> list:
-  docs = parse_corpus()
-  words_corpus = word_index()
-  return [doc_value(doc, words_corpus) for doc in docs]
- 
-#saber el numero de bit de cada palabra
-def word_index() -> dict:
-  docs = parse_corpus() #corpus
-  words_set = set() #palabras del corpus sin repetir
-  result = {}
-  i = 0
-
-  for doc in docs:
-    for word in doc:
-      if not word in words_set:
-        words_set.add(word)
-        result[word] = i
-        i += 1
-
-  return result
-
-#valor de un documento
-def doc_value(doc: list, words_corpus: dict) -> int:
-  words_set = set() #palabras del documento sin repetir
-  result = 0
-  
-  for word in doc:
-    if not word in words_set:
-      words_set.add(word)
-  
-  for word in words_set:
-    result += 2 ** words_corpus[word]
-  
-  return result
+#print(parse_corpus()[0])
--- src/code/process_query.py
+++ src/code/process_query.py
@@ -1,18 +1,12 @@
 #dependencias
 from lex_process import remove_stopwords_spacy, morphological_reduction_spacy
-from process_corpus import word_index, doc_value
+from utils import list_to_set
+from process_corpus import load
 
 #devolver la query parseada
 def parse_query(query: list) -> list:
-  return [clean_query(porcion) for porcion in query if len(porcion) != 0]
+  return list(map(list_to_set, [clean_query(porcion) for porcion in query if len(porcion) != 0]))
 
 #eliminar stopwords y signos de puntuacion
 def clean_query(query_porcion: list) -> list:
   return morphological_reduction_spacy(remove_stopwords_spacy(query_porcion))
-
-#devolver los documentos de la query con sus respectivos valores en bits
-def query_porcion_values(query: list) -> list:
-  parts = parse_query(query)
-  words_corpus = word_index()
-  return [doc_value(doc, words_corpus) for doc in parts]
- 
